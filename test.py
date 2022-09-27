eed19b72ecc33167974a85eb0742b09f867c35e2
qqaazz0222 <qqaazz0222@naver.com>
2022-09-27 20:19:23 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,2 @@
-s = input()
-i = {}
-d = {}
-state = False
-for i in range(len(s)):
-    if d.get(s[i]) == None:
-        d[s[i]] = 1
-    else:
-        d[s[i]] += 1
-for i in d:
-    d[i]
-if state:
-    answer = 'abc'
-else:
-    answer = 'N'
-print(d)
-print(answer)
+arr = [[1,2],[2,3]]
+print(len(arr[0]))
