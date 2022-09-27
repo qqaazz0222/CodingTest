22c8352c565319c290563dc3172c4b00ba7d5f83
qqaazz0222 <qqaazz0222@naver.com>
2022-09-27 20:22:12 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-arr = [[1,2],[2,3]]
-print(len(arr[0]))
+answer = 0
+num = 6
+while True:
+    print(answer, num)
+    if answer < 500:
+        if num % 2 == 0:
+            num = num / 2
+        else:
+            num = num / 3 + 1
