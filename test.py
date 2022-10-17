3a3010cf37aef2fbbbddd9af6167c6dff8fb9f06
qqaazz0222 <qqaazz0222@naver.com>
2022-10-17 14:17:50 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-temp = [1, 2, 3]
-print(temp*2)
+def solution(n):
+    answer = 1
+    temp = 1
+    while True:
+        if temp >= n:
+            break
+        else:
+            temp *= answer
+            answer += 1
+
+    return answer
+
+
+print(solution(3628800))
+print(solution(7))
