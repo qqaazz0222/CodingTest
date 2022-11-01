83332a313389a7f006258ee1bda6877b059ef728
qqaazz0222 <qqaazz0222@naver.com>
2022-11-01 13:00:39 +0900

    테스트

--- test.py
+++ test.py
@@ -1,2 +1,24 @@
-a = [1, 2, 3, 4, 5]
-print(a[1:3])
+import math
+
+def solution(progresses, speeds):
+    stack = []
+    answer = []
+    for i in range(len(progresses)):
+        temp = math.ceil((100 - progresses[i])//speeds[i])
+        if len(stack) == 0:
+            stack.append(temp)
+        else:
+            before = stack.pop()
+            stack.append(before)
+            if before > temp:
+                stack.append(before)
+            else:
+                stack.append(temp)
+    
+    ss = list(dict.fromkeys(stack))
+    print(ss)
+    for i in ss:
+        answer.append(stack.count(i))
+    return answer
+
+print(solution([95], [3]))
