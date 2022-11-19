d59b0c4d9a2ca60c825646d3c6f7f43dac575311
qqaazz0222 <qqaazz0222@naver.com>
2022-11-19 12:37:27 +0900

    update test.py

--- test.py
+++ test.py
@@ -1,27 +1,6 @@
-import math
-
-
-def solution(progresses, speeds):
-    stack = []
-    answer = []
-    for i in range(len(progresses)):
-        temp = math.ceil((100 - progresses[i])/speeds[i])
-        print(temp)
-        if len(stack) == 0:
-            stack.append(temp)
-        else:
-            before = stack.pop()
-            stack.append(before)
-            if before > temp:
-                stack.append(before)
-            else:
-                stack.append(temp)
-
-    ss = list(dict.fromkeys(stack))
-    print(ss)
-    for i in ss:
-        answer.append(stack.count(i))
-    return answer
-
-
-print(solution([95, 94], [3, 3]))
+edges = [[0, 1], [0, 2], [1, 2], [2, 3], [2, 5], [3, 4], [3, 5]]
+graph = [[] for _ in range(6)]
+for a, b in edges:
+    graph[a].append(b)
+    graph[b].append(a)
+print(graph)
